778152b323c16ce492a30c572c05b4b82bb2f2b9
Benjamin Barbe <benjamin@pandasuite.com>
2021-12-14 17:25:05 +0100

    Low code example for filtering collection

--- src/index.js
+++ src/index.js
@@ -1,44 +1,41 @@
+import {
+  differenceBy,
+  flatten, get, groupBy, includes, keys, map, shuffle,
+} from 'lodash';
 import PandaBridge from 'pandasuite-bridge';
+
 import './index.css';
 
 let properties = null;
-let markers = null;
-
-function myInit() {
-  // const imageUrl = PandaBridge.resolvePath('my_image.png');
-  // PandaBridge.send('imageChanged');
-}
 
 PandaBridge.init(() => {
   PandaBridge.onLoad((pandaData) => {
     properties = pandaData.properties;
-    markers = pandaData.markers;
-
-    if (document.readyState === 'complete') {
-      myInit();
-    } else {
-      document.addEventListener('DOMContentLoaded', myInit, false);
-    }
   });
 
   PandaBridge.onUpdate((pandaData) => {
     properties = pandaData.properties;
-    markers = pandaData.markers;
   });
 
-  /* Markers */
+  PandaBridge.listen('search', ([args]) => {
+    const { source, limit, intruder } = properties;
+    const { symbole } = args;
 
-  PandaBridge.getSnapshotData(() => null);
+    if (source && symbole) {
+      const registre = groupBy(source, (item) => get(item, 'fields.registre'));
+      const nbToTake = Math.max(1, Math.floor((limit * (1 - intruder)) / keys(registre).length));
 
-  PandaBridge.setSnapshotData((pandaData) => {
-    // pandaData.data.id
-  });
+      const result = flatten(map(registre, (items) => shuffle(items.filter((item) => includes(get(item, 'fields.symbole'), symbole))).slice(0, nbToTake)));
+      const nbToFill = limit - result.length;
 
-  /* Actions */
+      const queryable = {
+        results: shuffle(result).concat(shuffle(differenceBy(source, result, 'id')).slice(0, nbToFill)),
+      };
 
-  PandaBridge.listen('changeColor', (args) => {
-  });
-
-  PandaBridge.synchronize('synchroImages', (percent) => {
+      PandaBridge.send('results', [queryable]);
+      PandaBridge.send(PandaBridge.UPDATED, {
+        queryable,
+      });
+    }
   });
 });
